112f3799ce12bd5090ed18b60b07e49042b64471
Roshan Jagadish <roshan10.rj@gmail.com>
2022-09-30 15:10:12 +0530

    Created components

---
+++ components/HotelSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HotelSection = () => {
+  return (
+    <div>HotelSection</div>
+  )
+}
+
+export default HotelSection
---
+++ components/TrainSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TrainSection = () => {
+  return (
+    <div>TrainSection</div>
+  )
+}
+
+export default TrainSection
